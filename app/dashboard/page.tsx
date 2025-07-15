3ac2fb1dfd602baa916b9d1fbc859ba852424ca6
João Pedro Medeiros <jpmdrsdev@gmail.com>
2025-07-14 21:11:56 -0300

    wip

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { useAuth } from "@/context/AuthContext";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+
 import { useEffect } from "react";
 
 export default function Dashboard() {
